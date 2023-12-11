8efaf2cc7568b9278c8e126aead75c92101b4a42
juanf1328 <juanf1328@gmail.com>
2023-12-11 20:37:34 -0300

    styles login

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import { Button, Input, Row, Col } from 'antd';
 import './App.css';
 
 class App extends React.Component {
- constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       language: 'english',
+      showLoginForm: false, // Nuevo estado para controlar la visibilidad del formulario de inicio de sesión
     };
- }
+  }
 
- changeLanguage = (language) => {
+  changeLanguage = (language) => {
     this.setState({ language });
- };
+  };
 
- render() {
-    const { language } = this.state;
+  showLoginForm = () => {
+    this.setState({ showLoginForm: true });
+  };
+
+  render() {
+    const { language, showLoginForm } = this.state;
 
     const loginPage = {
-      title: language === 'english' ? 'Welcome to atawall' : 'Bienvenido a atawall',
+      title: language === 'english' ? 'atawall' : 'atawall',
       buttonText: language === 'english' ? 'Log In' : 'Iniciar Sesión',
       registerText: language === 'english' ? 'Register' : 'Registrarme',
       needHelpText: language === 'english' ? 'Need Help?' : 'Necesitas ayuda?',
@@ -27,40 +32,65 @@ class App extends React.Component {
     return (
       <div className="login-container">
         <h1>{loginPage.title}</h1>
-        <Row>
-          <Col span={8}>
-            <Input placeholder={language === 'english' ? 'Email' : 'Correo electrónico'} />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <Input.Password placeholder={language === 'english' ? 'Password' : 'Contraseña'} />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <Button type="primary">{loginPage.buttonText}</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <Button>{loginPage.registerText}</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <p>{loginPage.needHelpText}</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <Button onClick={() => this.changeLanguage('english')}>ENG</Button>
-            <Button onClick={() => this.changeLanguage('spanish')}>ESP</Button>
-          </Col>
-        </Row>
+        <h2 className="subtitle">{language === 'english' ? 'Welcome' : 'Bienvenido'}</h2>
+        {showLoginForm && (
+          <>
+           <Row>
+  <Col span={8}>
+    <Input
+      style={{
+        width: '12rem',
+        marginBottom: '1rem',  // Ajusta la separación vertical entre los inputs
+        borderColor: '#1b1b1b',  // Cambia el color del borde al color del fondo
+      }}
+      placeholder={language === 'english' ? 'Email' : 'Correo electrónico'}
+    />
+  </Col>
+</Row>
+<Row>
+  <Col span={8}>
+    <Input.Password
+      style={{
+        width: '12rem',
+        marginBottom: '1rem',  // Ajusta la separación vertical entre los inputs
+        borderColor: '#1b1b1b',  // Cambia el color del borde al color del fondo
+      }}
+      placeholder={language === 'english' ? 'Password' : 'Contraseña'}
+    />
+  </Col>
+</Row>
+          </>
+        )}
+<Row>
+  <Col span={8}>
+    <div className="button-container">
+      <Button onClick={this.showLoginForm} style={{ width: '15rem', color: '#000', background: '#05c988', borderColor: '#05c988' }}>
+        {loginPage.buttonText}
+      </Button>
+    </div>
+    <div className="button-container">
+      <Button style={{ width: '15rem', marginTop: '10px', background: '#1b1b1b', color: '#fff', borderColor: '#fff' }}>{loginPage.registerText}</Button>
+    </div>
+  </Col>
+</Row>
+
+<Row>
+  <Col span={8}>
+    <p style={{ color: '#05c988', textDecoration: 'underline', textDecorationColor: '#05c988', width: '7rem', cursor: 'pointer', marginLeft: '2rem' }}>{loginPage.needHelpText}</p>
+  </Col>
+</Row>
+<Row>
+  <Col span={8}>
+    <div className="language-container">
+      <Button style={{ color:'#7c7c7c', background:'#1b1b1b', borderColor:'#1b1b1b' }} onClick={() => this.changeLanguage('english')} className="language-button">ENG</Button>
+      <div className="language-divider" />
+      <Button style={{ color:'#7c7c7c', background:'#1b1b1b', borderColor:'#1b1b1b' }} onClick={() => this.changeLanguage('spanish')} className="language-button">ESP</Button>
+    </div>
+  </Col>
+</Row>
       </div>
     );
- }
+  }
 }
 
 export default App;
