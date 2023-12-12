2f6be32a3df589cf72cae2346b20c454f0a93e6d
juanf1328 <juanf1328@gmail.com>
2023-12-11 21:35:28 -0300

    nueva estrucura y routing

--- src/App.js
+++ src/App.js
@@ -1,96 +1,18 @@
+// App.js
 import React from 'react';
-import { Button, Input, Row, Col } from 'antd';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import MainPage from './components/MainPage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      language: 'english',
-      showLoginForm: false, // Nuevo estado para controlar la visibilidad del formulario de inicio de sesión
-    };
-  }
-
-  changeLanguage = (language) => {
-    this.setState({ language });
-  };
-
-  showLoginForm = () => {
-    this.setState({ showLoginForm: true });
-  };
-
-  render() {
-    const { language, showLoginForm } = this.state;
-
-    const loginPage = {
-      title: language === 'english' ? 'atawall' : 'atawall',
-      buttonText: language === 'english' ? 'Log In' : 'Iniciar Sesión',
-      registerText: language === 'english' ? 'Register' : 'Registrarme',
-      needHelpText: language === 'english' ? 'Need Help?' : 'Necesitas ayuda?',
-    };
-
-    return (
-      <div className="login-container">
-        <h1>{loginPage.title}</h1>
-        <h2 className="subtitle">{language === 'english' ? 'Welcome' : 'Bienvenido'}</h2>
-        {showLoginForm && (
-          <>
-           <Row>
-  <Col span={8}>
-    <Input
-      style={{
-        width: '12rem',
-        marginBottom: '1rem',  // Ajusta la separación vertical entre los inputs
-        borderColor: '#1b1b1b',  // Cambia el color del borde al color del fondo
-      }}
-      placeholder={language === 'english' ? 'Email' : 'Correo electrónico'}
-    />
-  </Col>
-</Row>
-<Row>
-  <Col span={8}>
-    <Input.Password
-      style={{
-        width: '12rem',
-        marginBottom: '1rem',  // Ajusta la separación vertical entre los inputs
-        borderColor: '#1b1b1b',  // Cambia el color del borde al color del fondo
-      }}
-      placeholder={language === 'english' ? 'Password' : 'Contraseña'}
-    />
-  </Col>
-</Row>
-          </>
-        )}
-<Row>
-  <Col span={8}>
-    <div className="button-container">
-      <Button onClick={this.showLoginForm} style={{ width: '15rem', color: '#000', background: '#05c988', borderColor: '#05c988' }}>
-        {loginPage.buttonText}
-      </Button>
-    </div>
-    <div className="button-container">
-      <Button style={{ width: '15rem', marginTop: '10px', background: '#1b1b1b', color: '#fff', borderColor: '#fff' }}>{loginPage.registerText}</Button>
-    </div>
-  </Col>
-</Row>
-
-<Row>
-  <Col span={8}>
-    <p style={{ color: '#05c988', textDecoration: 'underline', textDecorationColor: '#05c988', width: '7rem', cursor: 'pointer', marginLeft: '2rem' }}>{loginPage.needHelpText}</p>
-  </Col>
-</Row>
-<Row>
-  <Col span={8}>
-    <div className="language-container">
-      <Button style={{ color:'#7c7c7c', background:'#1b1b1b', borderColor:'#1b1b1b' }} onClick={() => this.changeLanguage('english')} className="language-button">ENG</Button>
-      <div className="language-divider" />
-      <Button style={{ color:'#7c7c7c', background:'#1b1b1b', borderColor:'#1b1b1b' }} onClick={() => this.changeLanguage('spanish')} className="language-button">ESP</Button>
-    </div>
-  </Col>
-</Row>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,52 +1,101 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Button, Input, Row, Col } from 'antd';
 import './Login.css';
 
-async function loginUser(credentials) {
-  return fetch('http://localhost:8080/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(credentials)
-  })
-    .then(data => data.json());
-}
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: 'english',
+      showLoginForm: false, // Nuevo estado para controlar la visibilidad del formulario de inicio de sesión
+    };
+  }
 
-export default function Login({ setToken }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await loginUser({ username, password });
-      setToken(response.token);
-    } catch (error) {
-      console.error(error);
-    }
+  changeLanguage = (language) => {
+    this.setState({ language });
   };
 
-  return (
-    <div className="login-wrapper">
-      <h1>atawall</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          <p>Password</p>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">LogIn</button>
-      </form>
+  showLoginForm = () => {
+    this.setState({ showLoginForm: true });
+  };
+
+  render() {
+    const { language, showLoginForm } = this.state;
+
+    const loginPage = {
+      title: language === 'english' ? 'atawall' : 'atawall',
+      buttonText: language === 'english' ? 'Log In' : 'Iniciar Sesión',
+      registerText: language === 'english' ? 'Register' : 'Registrarme',
+      needHelpText: language === 'english' ? 'Need Help?' : 'Necesitas ayuda?',
+    };
+
+    return (
+      <div className="login-container">
+        <h1>{loginPage.title}</h1>
+        <h2 className="subtitle">{language === 'english' ? 'Welcome' : 'Bienvenido'}</h2>
+        {showLoginForm && (
+          <>
+           <Row>
+  <Col span={8}>
+    <Input
+      style={{
+        width: '14rem',
+        marginBottom: '1rem',
+        marginLeft:'-0.5rem',
+        height:'2rem',
+        marginTop:'1rem',  // Ajusta la separación vertical entre los inputs
+        borderColor: '#1b1b1b',  // Cambia el color del borde al color del fondo
+      }}
+      placeholder={language === 'english' ? 'Email' : 'Correo electrónico'}
+    />
+  </Col>
+</Row>
+<Row>
+  <Col span={8}>
+    <Input.Password
+      style={{
+        width: '14rem',
+        marginBottom: '1rem',
+        marginLeft:'-0.5rem',
+        height:'2rem',  // Ajusta la separación vertical entre los inputs
+        borderColor: '#1b1b1b',  // Cambia el color del borde al color del fondo
+      }}
+      placeholder={language === 'english' ? 'Password' : 'Contraseña'}
+    />
+  </Col>
+</Row>
+          </>
+        )}
+<Row>
+  <Col span={8}>
+    <div className="button-container">
+      <Button onClick={this.showLoginForm} style={{ width: '15rem', color: '#000', background: '#05c988', borderColor: '#05c988' }}>
+        {loginPage.buttonText}
+      </Button>
     </div>
-  );
+    <div className="button-container">
+      <Button style={{ width: '15rem', marginTop: '10px', background: '#1b1b1b', color: '#fff', borderColor: '#fff' }}>{loginPage.registerText}</Button>
+    </div>
+  </Col>
+</Row>
+
+<Row>
+  <Col span={8}>
+    <p style={{ color: '#05c988', textDecoration: 'underline', textDecorationColor: '#05c988', width: '7rem', cursor: 'pointer', marginLeft: '3rem' }}>{loginPage.needHelpText}</p>
+  </Col>
+</Row>
+<Row>
+  <Col span={8}>
+    <div className="language-container">
+      <Button style={{ color:'#7c7c7c', background:'#1b1b1b', borderColor:'#1b1b1b' }} onClick={() => this.changeLanguage('english')} className="language-button">ENG</Button>
+      <div className="language-divider" />
+      <Button style={{ color:'#7c7c7c', background:'#1b1b1b', borderColor:'#1b1b1b' }} onClick={() => this.changeLanguage('spanish')} className="language-button">ESP</Button>
+    </div>
+  </Col>
+</Row>
+      </div>
+    );
+  }
 }
 
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired
-};
+export default Login;
