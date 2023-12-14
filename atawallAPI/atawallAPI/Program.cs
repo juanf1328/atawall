0fdcb37282b6e00c03b9d7ba540ea3a9cb8e55b3
juanf1328 <jforni@SOFTWARE-FACTORY.ENCODESA.INTERNAL>
2023-12-14 09:28:21 -0300

    new api

---
+++ atawallAPI/atawallAPI/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+
+//Política de CORS para desarrollo:
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>//permisos
+{
+    builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
